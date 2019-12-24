fc42790d2d9eda701d73115b339fc9079f640a12
abhijeet3922 <abhijeetchar@gmail.com>
2019-12-24 16:22:44 +0530

    modified code, added logger

--- finbert_embedding/finbert_embedding.py
+++ finbert_embedding/finbert_embedding.py
@@ -6,6 +6,10 @@ from scipy.spatial.distance import cosine
 from pytorch_pretrained_bert import BertTokenizer, BertModel, BertForMaskedLM
 
 __author__ = 'Abhijeet Kumar'
+
+#Create and configure logger
+logging.basicConfig(filename='app.log', filemode='w',format='%(asctime)s %(message)s', level=logging.INFO)
+
 logger = logging.getLogger(__name__)
 
 class FinbertEmbedding(object):
@@ -19,24 +23,47 @@ class FinbertEmbedding(object):
             pre-trained BERT model
     """
 
-    def __init__(self, model_path=None, batch_size=256):
+    def __init__(self, model_path=None):
 
         if model_path is not None:
             self.model_path = model_path
         else:
             self.model_path = Path.cwd().parent/'finbertTRC2'
 
-        self.batch_size = batch_size
+        self.tokens = ""
+        self.sentence_tokens = ""
         self.tokenizer = BertTokenizer.from_pretrained(self.model_path)
         # Load pre-trained model (weights)
         self.model = BertModel.from_pretrained(self.model_path)
+        logger.info("Initialization Done !!")
 
-    def word_vector(self, text):
+    def process_text(self, text):
 
         marked_text = "[CLS] " + text + " [SEP]"
         # Tokenize our sentence with the BERT tokenizer.
         tokenized_text = self.tokenizer.tokenize(marked_text)
-        self.tokens = tokenized_text
+        return tokenized_text
+
+
+    def handle_oov(self, tokenized_text, word_embeddings):
+        embeddings = []
+        tokens = []
+        oov_len = 1
+        for token,word_embedding in zip(tokenized_text, word_embeddings):
+            if token.startswith('##'):
+                token = token[2:]
+                tokens[-1] += token
+                oov_len += 1
+                embeddings[-1] += word_embedding
+            else:
+                if oov_len > 1:
+                    embeddings[-1] /= oov_len
+                tokens.append(token)
+                embeddings.append(word_embedding)
+        return tokens,embeddings
+
+
+    def eval_fwdprop_finbert(self, tokenized_text):
 
         # Mark each of the tokens as belonging to sentence "1".
         segments_ids = [1] * len(tokenized_text)
@@ -49,10 +76,18 @@ class FinbertEmbedding(object):
 
         # Put the model in "evaluation" mode, meaning feed-forward operation.
         self.model.eval()
-
         # Predict hidden states features for each layer
         with torch.no_grad():
             encoded_layers, _ = self.model(tokens_tensor, segments_tensors)
+
+        return encoded_layers
+
+
+    def word_vector(self, text, handle_oov=True, filter_extra_tokens=True):
+
+        tokenized_text = self.process_text(text)
+
+        encoded_layers = self.eval_fwdprop_finbert(tokenized_text)
 
         # Concatenate the tensors for all layers. We use `stack` here to
         # create a new dimension in the tensor.
@@ -62,43 +97,39 @@ class FinbertEmbedding(object):
         token_embeddings = token_embeddings.permute(1,0,2)
 
         # Stores the token vectors, with shape [22 x 768]
-        token_vecs_sum = []
+        word_embeddings = []
+        logger.info("Summing last 4 layers for each token")
         # For each token in the sentence...
         for token in token_embeddings:
 
             # `token` is a [12 x 768] tensor
             # Sum the vectors from the last four layers.
             sum_vec = torch.sum(token[-4:], dim=0)
-            # Use `sum_vec` to represent `token`.
-            token_vecs_sum.append(sum_vec)
 
-        return token_vecs_sum
+            # Use `sum_vec` to represent `token`.
+            word_embeddings.append(sum_vec)
+
+        self.tokens = tokenized_text
+        if filter_extra_tokens:
+            # filter_spec_tokens: filter [CLS], [SEP] tokens.
+            word_embeddings = word_embeddings[1:-1]
+            self.tokens = tokenized_text[1:-1]
+
+        if handle_oov:
+            self.tokens, word_embeddings = self.handle_oov(self.tokens,word_embeddings)
+        logger.info(self.tokens)
+        logger.info("Shape of Word Embeddings = %s",str(len(word_embeddings)))
+        return word_embeddings
 
 
 
     def sentence_vector(self,text):
 
-        marked_text = "[CLS] " + text + " [SEP]"
-
-        # Tokenize our sentence with the BERT tokenizer.
-        tokenized_text = self.tokenizer.tokenize(marked_text)
-
-        # Mark each of the 22 tokens as belonging to sentence "1".
-        segments_ids = [1] * len(tokenized_text)
-
-        # Map the token strings to their vocabulary indeces.
-        indexed_tokens = self.tokenizer.convert_tokens_to_ids(tokenized_text)
-
-        # Convert inputs to PyTorch tensors
-        tokens_tensor = torch.tensor([indexed_tokens])
-        segments_tensors = torch.tensor([segments_ids])
-
-        # Put the model in "evaluation" mode, meaning feed-forward operation.
-        self.model.eval()
-
-        # Predict hidden states features for each layer
-        with torch.no_grad():
-            encoded_layers, _ = self.model(tokens_tensor, segments_tensors)
+        logger.info("Taking last layer embedding of each word.")
+        logger.info("Mean of all words for sentence embedding.")
+        tokenized_text = self.process_text(text)
+        self.sentence_tokens = tokenized_text
+        encoded_layers = self.eval_fwdprop_finbert(tokenized_text)
 
         # `encoded_layers` has shape [12 x 1 x 22 x 768]
         # `token_vecs` is a tensor with shape [22 x 768]
@@ -106,16 +137,15 @@ class FinbertEmbedding(object):
 
         # Calculate the average of all 22 token vectors.
         sentence_embedding = torch.mean(token_vecs, dim=0)
+        logger.info("Shape of Sentence Embeddings = %s",str(len(sentence_embedding)))
         return sentence_embedding
 
 
 if __name__ == "__main__":
 
-    text = "Another PSU bank, Punjab National Bank which also reported numbers" \
+    text = "Another PSU bank, Punjab National Bank which also reported numbers " \
             "managed to see a slight improvement in asset quality."
 
     finbert = FinbertEmbedding()
     word_embeddings = finbert.word_vector(text)
-    print(len(word_embeddings))
     sentence_embedding = finbert.sentence_vector(text)
-    print(len(sentence_embedding))
